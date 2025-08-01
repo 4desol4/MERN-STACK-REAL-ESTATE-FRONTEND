755ebbb6c777393f6eb39ecd9d463c8a6be4900e
4desol4 <adeleyeshola0@gmail.com>
2025-08-01 20:35:34 +0100

    Initial commit - MERN stack app

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Contact.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import {HiChatBubbleBottomCenter} from 'react-icons/hi2'
+const Contact = () => {
+  return (
+    <div id="contact-us" className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contact Us</span>
+          <span className="primaryText">Easy to contact us</span>
+          <span className="secondaryText">
+            We always ready to help by providijng the best services for you. We
+            beleive a good blace to live can make your life better{" "}
+          </span>
+
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">103-213-7431</span>
+                  </div>
+                </div>
+                <a href="https://services.zangi.com/dl/conversation/1032137431">
+                <div className="flexCenter button">Chat now</div>
+                </a>
+              </div>
+            </div>
+
+            {/* second row */}
+            <div className="flexStart row"> 
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Email</span>
+                    <span className="secondaryText">Email us now</span>
+                  </div>
+                </div>
+                <a href="mailto:presidiorealestate9@gmail.com">
+                <div className="flexCenter button">Email now</div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flexEnd c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
