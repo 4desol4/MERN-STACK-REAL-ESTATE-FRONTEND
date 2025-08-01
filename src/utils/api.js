0d2f8f5e1a5f1719fbffc0204bc2491998d23ce2
4desol4 <93643424+4desol4@users.noreply.github.com>
2025-08-01 23:24:56 +0100

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  baseURL: "https://mern-stack-real-estate-backend-jd8w.onrender.com/api",
 });
 
 export const getAllProperties = async () => {
