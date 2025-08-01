900502a48ab3ab9b95751d1780be7a0b6521f6b6
4desol4 <93643424+4desol4@users.noreply.github.com>
2025-08-01 21:22:04 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-j65vry3p5rpawtf4.us.auth0.com"
      clientId="vUmfdDoNylN3hplt9u4iV7XPeRUeTRi1"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173/",
+      redirect_uri: "https://mern-stack-real-estate-frontend.vercel.app/",
       audience:"http://localhost:8000",
       scope:"openid profile email"
      }}
